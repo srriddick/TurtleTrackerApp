ef15ec4d0e541c1cf338878078ec2cc0cbf23f22
srriddick <sage.riddick@duke.edu>
2020-09-24 12:31:55 -0400

    Parses a pasted data line into variables

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,3 +8,18 @@
 # Author: Sage Riddick (sage.riddick@duke.edu)
 # Date:   Fall 2020
 #--------------------------------------------------------------
+
+#Pretend we read one line of data
+lineString = '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
+#Split the string into a List of data items
+lineData = lineString.split()
+
+#Extract items in List into variables
+record_id=lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData [6]
+obs_long = lineData [7]
+
+#Print location of sara
+print(f"Record {record_id} Indicated Sara was seen at lat:{obs_lat}, lon:{obs_long} on {obs_date}")
